bb5c3156ed1ce78fc734058b55a0b5fb573be69b
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-04-18 19:04:14 +0200

    [Menu component] add google font and logo OK

--- lib/screens/components/menu_component.dart
+++ lib/screens/components/menu_component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MenuComponent extends StatelessWidget {
   const MenuComponent({Key key}) : super(key: key);
@@ -8,14 +9,28 @@ class MenuComponent extends StatelessWidget {
     return Drawer(
       child: Column(children: <Widget>[
         DrawerHeader(
-          child: Center(child: Text("Flutter IN French")),
+          // child: Center(child: Text("Flutter IN French ")),
+          child: Image.asset('assets/logo_header.png'),
         ),
         Expanded(
           child: ListView(
             children: <Widget>[
-              Text("Accueil"),
-              Text("En savoir plus"),
+              ListTile(
+                  title: Text(
+                "Accueil",
+                style: GoogleFonts.robotoSlab(fontSize: 22.0),
+              )),
+              ListTile(
+                  title: Text(
+                "En savoir plus",
+                style: GoogleFonts.robotoSlab(fontSize: 22.0),
+              )),
             ],
+          ),
+        ),
+        Container(
+          child: Text(
+            "Copyright 2021 - Tous droits réservés",
           ),
         )
       ]),

77022508ef44f5596794e04452b614f08893e120
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-04-19 17:33:17 +0200

    [Controller] add class article controller

--- lib/controllers/article_controller.dart
+++ lib/controllers/article_controller.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:infrench2021_2/models/article_model.dart';
 class ArticleController extends ChangeNotifier {
   final Firestore _firestore = Firestore.instance;
 
-  get getArticles => _firestore
+  Future  getArticles async { 
+    QuerySnapshot _dataFirestore = await  _firestore
       .collection("articles")
       .orderBy("date", descending: true)
       .getDocuments();
+
+      List<ArticleModel> articlesData =
+      _dataFirestore
+      .documents
+      .map((article) => ArticleModel(
+        title: article.data["name"],
+       description: article.data["description"],
+        date: article.data["date"],
+         imageUrl: article.data["preview"]))
+      .toList();
+
+      
+  }
 }

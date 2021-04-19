f77a76102eccc239f19d432bf629805a1adf164d
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-04-19 11:10:46 +0200

    [Main screen] add provider ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:infrench2021_2/controllers/article_controller.dart';
 import 'package:infrench2021_2/screens/article_screen.dart';
 import 'package:infrench2021_2/screens/contact_screen.dart';
 import 'package:infrench2021_2/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(FlutterINFrenchApp());
 
@@ -13,21 +15,24 @@ class FlutterINFrenchApp extends StatelessWidget {
   Widget build(BuildContext context) {
     precacheImage(AssetImage("assets/logo_header.png"),
         context); // load image before show it
-    return MaterialApp(
-      title: "FlutterINFrench",
-      initialRoute: "/",
-      routes: {
-        "/": (context) => HomeScreen(),
-        "/contact": (context) => ContactScreen(),
-        "/article": (context) => ArticleScreen(),
-      },
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-            color: Colors.blue[800],
-            textTheme: TextTheme(
-                headline6: GoogleFonts.robotoSlab(
-              fontSize: 30.0,
-            ))),
+    return ChangeNotifierProvider(
+      create: (context) => ArticleController(),
+      child: MaterialApp(
+        title: "FlutterINFrench",
+        initialRoute: "/",
+        routes: {
+          "/": (context) => HomeScreen(),
+          "/contact": (context) => ContactScreen(),
+          "/article": (context) => ArticleScreen(),
+        },
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+              color: Colors.blue[800],
+              textTheme: TextTheme(
+                  headline6: GoogleFonts.robotoSlab(
+                fontSize: 30.0,
+              ))),
+        ),
       ),
     );
   }
